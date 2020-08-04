3f3e50d9860f76597bed3f667fbd9fd4233c49f4
bkthomps <bkthomps@uwaterloo.ca>
2020-08-04 17:48:52 -0400

    Add fitness calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 #include <memory>
 #include <unordered_map>
@@ -5,17 +6,41 @@
 #include "constants.h"
 #include "expressions.h"
 
+int calculateCombinations(int length) {
+    int ret = 1;
+    for (int i = 0; i < length; i++) {
+        ret *= 2;
+    }
+    return ret;
+}
+
+double fitness(Expr* head, int addressCount, const std::vector<const std::string>& options) {
+    assert(options.size() <= 16);
+    unsigned long startIndex = 16 - options.size();
+    int combinations = calculateCombinations(options.size());
+    std::unordered_map<std::string, bool> mux{};
+    double correct = 0;
+    for (int i = 0; i < combinations; i++) {
+        std::string str = std::bitset<16>(i).to_string();
+        for (unsigned long j = 0; j < options.size(); j++) {
+            mux.emplace(options[j], str[startIndex + j] - '0');
+        }
+        auto currentAddress = str.substr(startIndex, addressCount);
+        unsigned long address = std::bitset<16>(currentAddress).to_ulong();
+        bool actualTruth = str[startIndex + addressCount + address] == '1';
+        bool predictedTruth = head->evaluate(mux);
+        if (actualTruth == predictedTruth) {
+            correct++;
+        }
+        mux.clear();
+    }
+    return correct / combinations;
+}
+
 int main() {
-    const std::unordered_map<std::string, bool> mux6 = {
-            {"a0", false},
-            {"a1", false},
-            {"d0", false},
-            {"d1", false},
-            {"d2", false},
-            {"d3", false},
-    };
     std::vector<const std::string> optionsMux6 = {"a0", "a1", "d0", "d1", "d2", "d3"};
     std::unique_ptr<Expr> first = randomNode(optionsMux6, initialDepth);
+    std::cout << fitness(first.get(), 2, optionsMux6) << std::endl;
     std::unique_ptr<Expr> second = randomNode(optionsMux6, initialDepth);
     auto tuple = performRecombination(first.get(), second.get());
     std::cout << first->prettyPrint() << std::endl;
